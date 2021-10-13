b9a55a95288d8ba7129a01646919c581c1a4daa4
Eric Antunes <antunesv.eric@gmail.com>
2021-10-13 16:58:45 -0300

    primeiro commit

---
+++ src/test/java/com/antuneseric/workshopspring/WorkshopspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antuneseric.workshopspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
